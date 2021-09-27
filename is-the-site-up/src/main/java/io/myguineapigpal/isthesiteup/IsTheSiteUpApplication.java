44eb08b520b568361ffca7849048081ce709c260
jubielub <jubielub@gmail.com>
2021-09-26 23:53:03 -0700

    begin setting up spring

---
+++ is-the-site-up/src/main/java/io/myguineapigpal/isthesiteup/IsTheSiteUpApplication.java
@@ -0,0 +1,13 @@
+package io.myguineapigpal.isthesiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTheSiteUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsTheSiteUpApplication.class, args);
+	}
+
+}
---
+++ is-the-site-up/src/test/java/io/myguineapigpal/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myguineapigpal.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
